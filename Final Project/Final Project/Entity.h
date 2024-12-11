7c35686ee06139c1fdc4f289f415097524587234
Peter Perry <arthur.f.perry@gmail.com>
2024-12-11 13:59:17 -0500

    Final

--- Final Project/Final Project/Entity.h
+++ Final Project/Final Project/Entity.h
@@ -1,7 +1,7 @@
 /**
 * Author: Peter Perry
-* Assignment: Platformer
-* Date due: 2024-11-26, 11:59pm
+* Assignment: Final Project
+* Date due: 2024-12-11, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
